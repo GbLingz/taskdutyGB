6b4998b1d2f82b55b8b751f807ce64e2359f7fd2
Gbolahan <oshotekungbolahan@gmail.com>
2024-04-15 10:42:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './Layout/Navbar'
+import Home from './pages/Home'
+import AllTask from './pages/AllTask'
+import NewTask from './pages/NewTask'
+import EditTask from './pages/EditTask'
+
+
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Navbar/>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/alltask" element={<AllTask />} />
+    <Route path="/newtask" element={<NewTask />} />
+    <Route path="/edittask" element={<EditTask />} />
+    
+   
+
+    </Routes>
+    </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App
